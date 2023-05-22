c23abb5f2d8446ef2d2dd80a2da4e14b3fa49197
karthik g k <kar221694@gmail.com>
2023-05-22 21:21:16 +0530

    corrected errors

--- my-app/app/search/page.js
+++ my-app/app/search/page.js
@@ -3,28 +3,57 @@
 import {useState} from 'react';
 import {useProvider, useSigner, useContract} from "wagmi";
 import { landContractABI } from '@/constants';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
 
 function search() {
- const[data, setData] = useState("")
+ const[address, setAddress] = useState("");
+ const [data, setData] = useState({})
 
  //////create interface for contract ////////////
 
  const provider = useProvider()
- const signer = useSigner()
+ const {data:signer} = useSigner()
 
  const landContract = useContract({
-    addressOrName:data,
+    addressOrName:address,
     contractInterface:landContractABI,
-    signerOrProvider:signer | provider
+    signerOrProvider:signer ||  provider
  })
 
 
  function handleOnChange(e){
-    setData(e.target.value);
+    setAddress(e.target.value);
  }
 
-function handleData(){
-  
+async function handleData(){
+
+  try{
+       let country = await landContract.country();
+       let state = await landContract.state();
+       let district = await landContract.district();
+       let village = await landContract.village();
+       let landaddress = await landContract.landaddress();
+       let length = await landContract.length();
+       let width = await landContract.width();
+       let owner = await landContract.currentOwner()
+       setData({
+        country: country,
+        state: state,
+        district: district,
+        village: village,
+        landaddress: landaddress,
+        length:length,
+        width:width,
+        owner: owner
+       })
+
+  }
+  catch(err){
+       toast.error("Your address is not valid")
+  }
 }
 
   return (
@@ -34,6 +63,19 @@ function handleData(){
         <input className="shadow appearance-none border rounded w-[40vw] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name="address" placeholder='Enter contract address' onChange={handleOnChange} autoComplete="off"/>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleData} >search</button>
     </div>
+    {
+    Object.keys(data).length > 0?
+    <div className='w-[80vw] h-[20vh] mx-auto  flex flex-col  font-bold text-white'>
+        <h1>{`COUNTRY - ${data.country}`}</h1>
+        <h1>{`STATE - ${data.state}`}</h1>
+        <h1>{`DISTRICT - ${data.district}`}</h1>
+        <h1>{`VILLAGE - ${data.village}`}</h1>
+        <h1>{`LAND ADDRESS - ${data.landaddress}`}</h1>
+        <h1>{`LENGTH - ${data.length} in feets`}</h1>
+        <h1>{`WIDTH - ${data.width} in feets`}</h1>
+        <h1>{`OWNER - ${data.owner}`}</h1>
+    </div>:""
+}
     </div>
   )
 }
